fc4b46862c245f314b884508707dbcd2d4adc484
mohamed tantawy <mtantawy2025@gmail.com>
2024-07-16 22:26:09 +0300

    dockerize the application and modifing properties url

--- src/main/resources/scripts/scripts.sql
+++ src/main/resources/scripts/scripts.sql
@@ -1,3 +1,8 @@
+---  database creation then schema creation must executed  before running the application
 CREATE DATABASE triptekker;
 
+--- check connection then create schema from the db UI if it is not created
 CREATE SCHEMA triptekker;
+
+---  after registering new user run this query
+SELECT * FROM triptekker.user;
